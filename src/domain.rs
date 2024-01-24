752d892d2a5e75e2d83ca11350fcd933b6e8d787
dogenkigen <mlaskowsk@gmail.com>
2024-01-24 17:30:24 +0100

    Initial parser draft

--- src/domain.rs
+++ src/domain.rs
@@ -1,29 +1,47 @@
 use std::collections::HashMap;
+use std::str::FromStr;
 
-struct TestCase {
-    name: String,
-    request: Request,
-    response: Response,
+#[derive(Debug, Clone)]
+pub struct TestCase {
+    pub request: Request,
+    pub response: Response,
 }
 
 // TODO consider using client's enums?
-enum HttpMethod {
+#[derive(Debug, Clone, PartialEq)]
+pub enum HttpMethod {
     GET,
     POST,
     PUT,
     DELETE,
 }
 
-struct Request {
-    // TODO add cert
-    http_method: HttpMethod,
-    headers: HashMap<String, String>,
-    url: String,
-    body: String,
+impl FromStr for HttpMethod {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_uppercase().as_str() {
+            "GET" => Ok(HttpMethod::GET),
+            "POST" => Ok(HttpMethod::POST),
+            "PUT" => Ok(HttpMethod::PUT),
+            "DELETE" => Ok(HttpMethod::DELETE),
+            _ => Err(format!("{} is not a valid http method", s)),
+        }
+    }
 }
 
-struct Response {
-    code: i32,
-    headers: HashMap<String, String>,
-    body: String,
+#[derive(Debug, Clone)]
+pub struct Request {
+    // TODO add cert
+    pub http_method: HttpMethod,
+    pub headers: HashMap<String, String>,
+    pub url: String,
+    pub body: Option<String>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Response {
+    pub code: i32,
+    pub headers: HashMap<String, String>,
+    pub body: Option<String>,
 }
---
+++ src/parser.rs
@@ -0,0 +1,137 @@
+use std::collections::HashMap;
+use std::fs;
+use std::str::{FromStr, Lines};
+
+use markdown::mdast::{Code, Node};
+use markdown::to_mdast;
+
+use crate::domain::{HttpMethod, Request, Response, TestCase};
+
+const DOC_ASSERT_REQUEST: &str = "docassertrequest";
+const DOC_ASSERT_RESPONSE: &str = "docassertresponse";
+
+fn parse(path: String) -> Result<Vec<TestCase>, String> {
+    let test = fs::read_to_string(path)
+        .map_err(|e| e.to_string())
+        .map(|content| to_mdast(content.as_str(), &markdown::ParseOptions::default()))
+        .map(|ast| get_code_blocks(ast.unwrap()))
+        .iter()
+        .flat_map(|v| v.iter())
+        .fold((vec![], vec![]), |(mut reqs, mut resps), code| {
+            if code.lang == Some(DOC_ASSERT_REQUEST.to_string()) {
+                reqs.push(get_request(code).unwrap());
+            } else if code.lang == Some(DOC_ASSERT_RESPONSE.to_string()) {
+                resps.push(get_response(code).unwrap());
+            }
+            (reqs, resps)
+        });
+    Ok(test.0.iter().zip(test.1.iter()).map(|(req, resp)| {
+        TestCase {
+            request: req.clone(),
+            response: resp.clone(),
+        }
+    }).collect::<Vec<TestCase>>())
+}
+
+fn get_request(code: &Code) -> Result<Request, String> {
+    let mut lines = code.value.lines();
+
+    // Parse HTTP method and URL
+    let parts = lines.next().ok_or("Request line not found")
+        .map(|line| line.split_whitespace().collect::<Vec<&str>>())?;
+    if parts.len() != 2 {
+        return Err(format!("Invalid request line {}", parts.join(" ")));
+    }
+
+    let (headers, body) = get_headers_and_body(lines)?;
+
+    Ok(Request {
+        http_method: HttpMethod::from_str(parts[0])?,
+        url: parts[1].to_string(),
+        headers,
+        body,
+    })
+}
+
+fn get_headers_and_body(mut lines: Lines) -> Result<(HashMap<String, String>, Option<String>), String> {
+    let mut headers = HashMap::new();
+    let mut body = String::new();
+    for line in &mut lines {
+        if body.is_empty() && line.contains(":") && !line.contains("{") {
+            let header_parts = line.split(":").map(|s| s.trim()).collect::<Vec<&str>>();
+            if header_parts.len() != 2 {
+                return Err(format!("Invalid header line {}", line));
+            }
+            headers.insert(header_parts[0].to_string(), header_parts[1].to_string());
+            continue;
+        }
+        body.push_str(line);
+    }
+    let body = if body.is_empty() {
+        None
+    } else {
+        Some(body.chars().filter(|c| !c.is_whitespace()).collect::<String>())
+    };
+    Ok((headers, body))
+}
+
+fn get_response(code: &Code) -> Result<Response, String> {
+    let mut lines = code.value.lines();
+
+    // Parse HTTP method and URL
+    let parts = lines.next().ok_or("Response code line not found")
+        .map(|line| line.split_whitespace().collect::<Vec<&str>>())?;
+    if parts.len() != 2 {
+        return Err(format!("Invalid response code line {}", parts.join(" ")));
+    }
+    let http_code = parts[1].parse::<i32>().map_err(|_| "Invalid HTTP code".to_string())?;
+
+    let (headers, body) = get_headers_and_body(lines)?;
+
+    Ok(Response {
+        code: http_code,
+        headers,
+        body,
+    })
+}
+
+fn get_code_blocks(node: Node) -> Vec<Code> {
+    let mut code_blocks = vec![];
+    match node {
+        Node::Code(code) => {
+            if let Some(lang) = &code.lang {
+                if lang == DOC_ASSERT_REQUEST || lang == DOC_ASSERT_RESPONSE {
+                    code_blocks.push(code);
+                }
+            }
+        }
+        _ => {
+            node.children().iter().flat_map(|v| v.iter()).for_each(|child| {
+                code_blocks.append(&mut get_code_blocks(child.clone()));
+            });
+        }
+    }
+    code_blocks
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::parse;
+
+    #[test]
+    fn test_parse() {
+        let result = parse("README.md".to_string());
+        assert!(result.is_ok());
+        let test_cases = result.unwrap();
+        assert_eq!(test_cases.len(), 1);
+        // request
+        assert_eq!(test_cases[0].request.http_method, crate::domain::HttpMethod::POST);
+        assert_eq!(test_cases[0].request.url, "/api/user");
+        assert_eq!(test_cases[0].request.headers.get("Content-Type").unwrap(), "application/json");
+        assert_eq!(test_cases[0].request.body.as_ref().unwrap(), "{\"name\":\"test\"}");
+        // response
+        assert_eq!(test_cases[0].response.code, 201);
+        assert_eq!(test_cases[0].response.headers.get("Content-Type").unwrap(), "application/json");
+        assert_eq!(test_cases[0].response.body.as_ref().unwrap(), "{\"id\":1,\"name\":\"test\"}");
+    }
+}
