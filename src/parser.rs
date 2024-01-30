24c25a3208a04b183c55e3ab7ed8143e0fd6b97b
Maciej Laskowski <mlaskowsk@gmail.com>
2024-01-30 10:36:36 +0100

    Revrite the parser so it supports ignoring paths (#12)

--- src/parser.rs
+++ src/parser.rs
@@ -2,48 +2,71 @@ use std::collections::HashMap;
 use std::fs;
 use std::str::{FromStr, Lines};
 
-use markdown::mdast::{Code, Node};
-use markdown::to_mdast;
-
 use crate::domain::{HttpMethod, Request, Response, TestCase};
 
-const DOC_ASSERT_REQUEST: &str = "docassertrequest";
-const DOC_ASSERT_RESPONSE: &str = "docassertresponse";
+const DOC_ASSERT_REQUEST: &str = "```docassertrequest";
+const DOC_ASSERT_RESPONSE: &str = "```docassertresponse";
+const IGNORE_PREFIX: &str = "[ignore]";
 
 fn parse(path: String) -> Result<Vec<TestCase>, String> {
-    fs::read_to_string(path)
-        .map_err(|e| e.to_string())
-        .and_then(|content| to_mdast(content.as_str(), &markdown::ParseOptions::default()))
-        .map(|ast| get_code_blocks(ast))
-        .and_then(|codes| {
-            let (reqs, resps) = codes.iter()
-                .fold((vec![], vec![]), |(mut reqs, mut resps), code| {
-                    if code.lang == Some(DOC_ASSERT_REQUEST.to_string()) {
-                        reqs.push(get_request(code));
-                    } else if code.lang == Some(DOC_ASSERT_RESPONSE.to_string()) {
-                        resps.push(get_response(code));
-                    }
-                    (reqs, resps)
-                });
-            reqs.into_iter().collect::<Result<Vec<Request>, String>>()
-                .and_then(|reqs| {
-                    resps.into_iter()
-                        .collect::<Result<Vec<Response>, String>>()
-                        .map(|resps| (reqs, resps))
-                })
+    let (mut requests, mut responses) = (vec![], vec![]);
+    let binding = fs::read_to_string(path).map_err(|e| e.to_string())?;
+    let mut lines = binding.lines();
+    while let Some(line) = lines.next() {
+        // todo refactor to push req/resp as func?
+        if line.starts_with(DOC_ASSERT_REQUEST) {
+            requests.push(get_request(get_code(&mut lines))?);
+        }
+        if line.starts_with(DOC_ASSERT_RESPONSE) {
+            responses.push(get_response(get_code(&mut lines))?);
+        }
+        if line.starts_with(IGNORE_PREFIX) {
+            if responses.is_empty() || responses.len() != requests.len() {
+                return Err(format!("Misplaced ignore {}", line));
+            }
+            let l = responses.len();
+            responses[l - 1].ignore_paths.push(get_ignore_path(line)?);
+        }
+    }
+    if requests.len() != responses.len() {
+        return Err(
+            format!(
+                "There is {} requests and {} responses but you need equal number of both",
+                requests.len(),
+                responses.len())
+        );
+    }
+    let test_cases = requests.iter()
+        .zip(responses.iter())
+        .map(|(req, resp)| {
+            TestCase { request: req.clone(), response: resp.clone() }
         })
-        .map(|(reqs, resps)| {
-            reqs.iter().zip(resps.iter()).map(|(req, resp)| {
-                TestCase {
-                    request: req.clone(),
-                    response: resp.clone(),
-                }
-            }).collect::<Vec<TestCase>>()
-        })
+        .collect::<Vec<TestCase>>();
+    Ok(test_cases)
 }
 
-fn get_request(code: &Code) -> Result<Request, String> {
-    let mut lines = code.value.lines();
+fn get_code(lines: &mut Lines) -> String {
+    let mut buff = String::new();
+    while let Some(line) = lines.next() {
+        if line.starts_with("```") {
+            break;
+        }
+        buff.push_str(format!("{}\n", line).as_str());
+    }
+    return buff;
+}
+
+fn get_ignore_path(s: &str) -> Result<String, String> {
+    let no_whitespace = s.chars().filter(|c| !c.is_whitespace()).collect::<String>();
+    let mut path = no_whitespace.split(":#").skip(1).collect::<String>();
+    path.remove(0);
+    path.pop();
+    // TODO add validation of ignore path
+    Ok(path)
+}
+
+fn get_request(code: String) -> Result<Request, String> {
+    let mut lines = code.lines();
 
     // Parse HTTP method and URL
     let parts = lines.next().ok_or("Request line not found")
@@ -58,6 +81,27 @@ fn get_request(code: &Code) -> Result<Request, String> {
         http_method: HttpMethod::from_str(parts[0])?,
         url: parts[1].to_string(),
         headers,
+        body,
+    })
+}
+
+fn get_response(code: String) -> Result<Response, String> {
+    let mut lines = code.lines();
+
+    // Parse HTTP method and URL
+    let parts = lines.next().ok_or("Response code line not found")
+        .map(|line| line.split_whitespace().collect::<Vec<&str>>())?;
+    if parts.len() != 2 {
+        return Err(format!("Invalid response code line {}", parts.join(" ")));
+    }
+    let http_code = parts[1].parse::<i32>().map_err(|_| "Invalid HTTP code".to_string())?;
+
+    let (headers, body) = get_headers_and_body(lines)?;
+
+    Ok(Response {
+        code: http_code,
+        headers,
+        ignore_paths: vec![],
         body,
     })
 }
@@ -84,46 +128,6 @@ fn get_headers_and_body(mut lines: Lines) -> Result<(HashMap<String, String>, Op
     Ok((headers, body))
 }
 
-fn get_response(code: &Code) -> Result<Response, String> {
-    let mut lines = code.value.lines();
-
-    // Parse HTTP method and URL
-    let parts = lines.next().ok_or("Response code line not found")
-        .map(|line| line.split_whitespace().collect::<Vec<&str>>())?;
-    if parts.len() != 2 {
-        return Err(format!("Invalid response code line {}", parts.join(" ")));
-    }
-    let http_code = parts[1].parse::<i32>().map_err(|_| "Invalid HTTP code".to_string())?;
-
-    let (headers, body) = get_headers_and_body(lines)?;
-
-    Ok(Response {
-        code: http_code,
-        headers,
-        ignore: vec![],
-        body,
-    })
-}
-
-fn get_code_blocks(node: Node) -> Vec<Code> {
-    let mut code_blocks = vec![];
-    match node {
-        Node::Code(code) => {
-            if let Some(lang) = &code.lang {
-                if lang == DOC_ASSERT_REQUEST || lang == DOC_ASSERT_RESPONSE {
-                    code_blocks.push(code);
-                }
-            }
-        }
-        _ => {
-            node.children().iter().flat_map(|v| v.iter()).for_each(|child| {
-                code_blocks.append(&mut get_code_blocks(child.clone()));
-            });
-        }
-    }
-    code_blocks
-}
-
 #[cfg(test)]
 mod tests {
     use crate::parser::parse;
@@ -143,5 +147,6 @@ mod tests {
         assert_eq!(test_cases[0].response.code, 201);
         assert_eq!(test_cases[0].response.headers.get("Content-Type").unwrap(), "application/json");
         assert_eq!(test_cases[0].response.body.as_ref().unwrap(), "{\"id\":1,\"name\":\"test\"}");
+        assert_eq!(test_cases[0].response.ignore_paths[0], "id".to_string());
     }
 }
